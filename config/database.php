75165bb96b559e8dbeb3be97656bcc203a3bdcee
Tamer <tamerinale@gmail.com>
2026-06-17 14:37:21 +0100

    config: update pgsql settings with DATABASE_URL support

--- config/database.php
+++ config/database.php
@@ -64,13 +64,18 @@ return [
         ],
 
         'pgsql' => [
-    'driver'   => 'pgsql',
-    'host'     => env('DB_HOST', '127.0.0.1'),
-    'port'     => env('DB_PORT', '5432'),
-    'database' => env('DB_DATABASE', ''),
-    'username' => env('DB_USERNAME', ''),
+    'driver' => 'pgsql',
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
     'password' => env('DB_PASSWORD', ''),
-    'sslmode'  => env('DB_SSLMODE', 'require'),  // مهم على Render
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'schema' => 'public',
+    'sslmode' => env('DB_SSLMODE', 'require'),
 ],
 
         'sqlsrv' => [
